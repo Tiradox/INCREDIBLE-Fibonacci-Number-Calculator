244da246449c7fa74f9a56450edffa72ef605189
Tiradox <57082550+Tiradox@users.noreply.github.com>
2024-04-09 18:27:52 +0400

    init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace INCREDIBLE_Fibonacci_Number_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
