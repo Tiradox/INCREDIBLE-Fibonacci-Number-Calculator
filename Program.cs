ef3caf5c62d0ba8ce271296be6c82b06dbc2bc85
Tiradox <kebermen1@gmail.com>
2024-04-09 20:09:53 +0400

    Changed the 'Fib(n)' function << Now it displays all numbers and counts them iteratively

--- Program.cs
+++ Program.cs
@@ -9,17 +9,26 @@ namespace INCREDIBLE_Fibonacci_Number_Calculator
             Console.Write("Введите n\n> ");
             int n = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write($"Вывод: {Fib(n - 1)}");
+            Fib(n - 1);
         }
 
         static long Fib(long n)
         {
-            if (n <= 1)
-            {
-                return n;
-            }
+            if (n == 0) return 0;
 
-            return Fib(n - 1) + Fib(n - 2);
+            int prev = 0;
+            int next = 1;
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(prev);
+
+                int sum = prev + next;
+                prev = next;
+                next = sum;
+            }
+            Console.WriteLine(next);
+
+            return next;
         }
     }
 }
