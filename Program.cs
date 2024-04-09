399d74a6ed70db3a14762227f9d3391c0db509a2
Tiradox <kebermen1@gmail.com>
2024-04-09 19:36:59 +0400

    Added function 'int Fib(n)' << It returns the nth Fibonacci number

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace INCREDIBLE_Fibonacci_Number_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите n\n> ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Вывод: {Fib(n)}");
+        }
+
+        static long Fib(long n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            return Fib(n - 1) + Fib(n - 2);
         }
     }
 }
